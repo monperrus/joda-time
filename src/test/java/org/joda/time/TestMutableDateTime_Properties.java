10844eb47c94ddb195e968afc836f81d0c0ba7d9
Martin Monperrus <martin.monperrus@gnieh.org>
2024-08-07 09:30:38 +0200

    Update TestMutableDateTime_Properties.java

--- src/test/java/org/joda/time/TestMutableDateTime_Properties.java
+++ src/test/java/org/joda/time/TestMutableDateTime_Properties.java
@@ -242,18 +242,6 @@ public class TestMutableDateTime_Properties extends TestCase {
         assertEquals(test.getChronology().months(), test.monthOfYear().getDurationField());
         assertEquals(test.getChronology().years(), test.monthOfYear().getRangeDurationField());
         assertEquals(9, test.monthOfYear().getMaximumTextLength(null));
-        assertEquals(3, test.monthOfYear().getMaximumShortTextLength(null));
-        test = new MutableDateTime(2004, 7, 9, 0, 0, 0, 0);
-        assertEquals("juillet", test.monthOfYear().getAsText(Locale.FRENCH));
-        assertEquals("juil.", test.monthOfYear().getAsShortText(Locale.FRENCH));
-        assertEquals(1, test.monthOfYear().getMinimumValue());
-        assertEquals(1, test.monthOfYear().getMinimumValueOverall());
-        assertEquals(12, test.monthOfYear().getMaximumValue());
-        assertEquals(12, test.monthOfYear().getMaximumValueOverall());
-        assertEquals(1, test.monthOfYear().getMinimumValue());
-        assertEquals(1, test.monthOfYear().getMinimumValueOverall());
-        assertEquals(12, test.monthOfYear().getMaximumValue());
-        assertEquals(12, test.monthOfYear().getMaximumValueOverall());
     }
 
     public void testPropertyAddMonthOfYear() {
